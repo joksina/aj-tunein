e47effd8c4dea1ed3c5ff770bfc26042184a2132
joksina <jksalaudeen@yahoo.com>
2023-07-31 09:20:31 -0700

    adding dev dependencies to fix an error

--- src/components/Stations/Stations.test.js
+++ src/components/Stations/Stations.test.js
@@ -1,6 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Stations from './Stations';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import * as router from 'react-router'
+const navigate = jest.fn()
+
+beforeEach(() => {
+  jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+})
 
 
 
@@ -17,5 +23,19 @@ describe('Stations', () => {
     const linkElement = screen.getByText(data[0].name);
     expect(linkElement).toBeInTheDocument();
   });
+
+  test('redirects when div is clicked', async () => {
+    let data = [{
+      id: 1,
+      imgUrl: "test",
+      name: "CNN",
+      Popularity: 5,
+      tags: ["music"]
+    }]
+    render(<Router><Stations data={data} /></Router>);
+    const div = await screen.findByText('CNN')
+    await fireEvent.click(div);
+    expect(navigate).toHaveBeenCalledTimes(1)
+  })
 
 })
